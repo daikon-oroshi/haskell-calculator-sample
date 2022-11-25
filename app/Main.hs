0863163ac6853de7e87c6816566e28f859370ec7
oroshi <oroshinekko@gmail.com>
2022-11-26 02:14:09 +0900

    build gi-gtk

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
-module Main (main) where
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+module Main where
 
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    Gtk.init Nothing
+    win <- new Gtk.Window [ #title := "calculator" ]
+    on win #destroy Gtk.mainQuit
+    button <- new Gtk.Button [ #label := "押して" ]
+    on button #clicked (set button [ #sensitive := False, #label := "OK" ] )
+    #add win button
+    #showAll win
+    Gtk.main
