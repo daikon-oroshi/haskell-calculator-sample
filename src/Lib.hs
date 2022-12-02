a348d0b15ce74b465e4e8f6e0004e5560e4dee10
oroshi <oroshinekko@gmail.com>
2022-12-03 04:25:38 +0900

    電卓の表示値object作成

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,39 @@
 module Lib
-    ( someFunc
+    (
+        DispVal,
+        toDispStr,
+        toNumber
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+data DispVal = DispVal {
+    is_positive :: Bool,
+    integer_part :: Int,
+    decimal_part :: Int
+}
+
+toDispStr :: DispVal -> String
+toDispStr dval
+    | integer_part dval == 0 && decimal_part dval == 0 = "0"
+    | otherwise =
+        if is_positive dval then "" else "-"
+            ++ show (integer_part dval)
+            ++ if decimal_part dval == 0
+                then ""
+                else "." ++ show (decimal_part dval)
+
+numOfDigit :: Int -> Int
+numOfDigit x
+    | x == 0 = 1
+    | x > 0 = 1
+    | otherwise = numOfDigit $ -x
+
+toNumber :: DispVal -> Double
+toNumber dval =
+    if is_positive dval then 1 else -1
+        * (
+            fromIntegral (integer_part dval)
+            + (
+                fromIntegral (decimal_part dval)
+                / 10 ^ numOfDigit (decimal_part dval)
+            )
+        )
